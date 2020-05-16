eac6066048ec3c0eb490f0e7fb400d0aad3bdf69
Dayna Leal <dayna.r.leal@gmail.com>
2020-05-16 17:30:44 -0500

    codeacademy syntax lesson 8/15

---
+++ learn-syntax.py
@@ -0,0 +1,43 @@
+# lesson 1
+my_name = "Dayna"
+print("Hello, " + my_name + "!")  # instead of console.log() or system.out.println()
+
+# -------------------------------------------------------------------------
+# lesson 2 taught about commenting out with a hash
+# like this
+
+# -------------------------------------------------------------------------
+# lesson 3 taught about print()
+print("Hello world (in python)")
+
+# -------------------------------------------------------------------------
+# lesson 4 taught about how both single and double quotes are used interchangably like js
+print("They're going home.")
+print('He said "Hello"')
+
+# -------------------------------------------------------------------------
+# lesson 5 teaches about variable structure
+# no characters other than underscore
+# numbers cannot be at beginning
+the_language = "Python"
+print(the_language)
+# now the_language can also be updated
+the_language = "Python 3"
+print(the_language)
+
+# -------------------------------------------------------------------------
+# lesson 6 discusses error types (uncomment to see)
+# this is a SyntaxError (mismatch quote)
+# print('This message has mismatched quote marks!")
+# and this is a NameError (doesnt recognize undeclared variable name)
+# print(Abracadabra)
+
+# -------------------------------------------------------------------------
+# lesson 7 is about numbers
+an_int = 5  # integers are whole numbers
+a_float = 5.5  # float values are decimal numbers
+# not type cast like Java, more like js so the variable is assigned type based on what value is declared
+
+# -------------------------------------------------------------------------
+# lesson 8: Calculations
+print(15 / 4)
