b6c5fde98e3958a1d38e25c0988dc79d9a23b34c
Dayna Leal <dayna.r.leal@gmail.com>
2020-06-04 23:53:03 -0500

    starting dictionaries lesson

---
+++ Dictionaries/dictionariesLesson.py
@@ -0,0 +1,7 @@
+# dictionaries seem kinda like js objects
+sensors = {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22}
+num_cameras = {"backyard": 6,  "garage": 2, "driveway": 1}
+
+print(sensors)
+
+# --------------------------------------------------------------------------------------------
--- Modules/dateTimes.py
+++ Modules/dateTimes.py
@@ -9,3 +9,10 @@ print(datetime.now() - datetime(2020, 10, 16))
 # --------------------------------------------------------------------------------
 
 parsed_date = datetime.strptime('Jun 3, 2020', '%b %d, %Y')
+print(parsed_date.month)
+
+
+# --------------------------------------------------------------------------------
+
+date_string = datetime.strftime(datetime.now(), '%b %d, %Y')
+print(date_string)
